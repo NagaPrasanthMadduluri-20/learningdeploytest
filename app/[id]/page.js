95a6dcd92774312c2448dbd91daca823a5ac44e9
NagaPrasanth <nagaprasanth.m@invensis-tech.com>
2025-01-17 14:43:01 +0530

    removed the validation

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -4,15 +4,7 @@ import { generatePageData } from '  @/data/Genericetestdata';
 import { notFound } from 'next/navigation';
 import React from 'react';
 
-function isValidCourseId(id) {
-    // Check if it matches the pattern 'course-' followed by 5 digits
-    const match = id.match(/^course-(\d{5})$/);
-    if (!match) return false;
-    
-    // Check if the number is between 1 and 20000
-    const number = parseInt(match[1]);
-    return number >= 1 && number <= 20000;
-  }
+
 
 export async function generateStaticParams() {
   const ids = Array.from({ length: 20000 }, (_, i) => ({
@@ -24,10 +16,7 @@ export async function generateStaticParams() {
 
 const Page = ({ params }) => {
 
-      // Show 404 if the ID format is invalid or out of range
-      if (!isValidCourseId(params.id)) {
-        notFound();
-      }
+ 
 
   // Use the imported generatePageData instead of getTemplateData
   const pageData = generatePageData(params.id);
